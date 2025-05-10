ff3299265c759531a77edf20754bbbf9ca4f8f55
SuvuYou <nazarsuvyliak1@gmail.com>
2025-05-10 23:11:20 +0300

    set health bar hearts on damage

--- Assets/_Scripts/UI/HealthBar.cs
+++ Assets/_Scripts/UI/HealthBar.cs
@@ -1,18 +1,36 @@
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HealthBar
+public class HealthBar: MonoBehaviour
 {
     [SerializeField] private HealthContextDataSO _healthContextDataSO;
-    // [SerializeField] private TextMeshProUGUI _healthText;
+    [SerializeField] private RectTransform _healthBarContainer;
+    [SerializeField] private Image _heartPrefab;
 
-    private void Awake() 
+    private void Start() 
     {
         _healthContextDataSO.HealthState.OnGetHit += OnGetHit;
     }
 
     private void OnGetHit(int decrease) 
     {
-        
+        RemoveAllChildren();
+        InstantiateHearts(_healthContextDataSO.HealthState.CurrentHealth);
+    }
+
+    private void RemoveAllChildren() 
+    {
+        foreach (Transform child in _healthBarContainer) 
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    private void InstantiateHearts(int numberOfHearts) 
+    {
+        for (int i = 0; i < numberOfHearts; i++)
+        {
+            Instantiate(_heartPrefab, _healthBarContainer);
+        }
     }
 }
